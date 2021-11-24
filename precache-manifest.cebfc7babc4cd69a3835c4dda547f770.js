e743535d2db19eee1234b6cfa56da0d1264597dc
Zalegaj <vlad.zalehay@mail.ru>
2021-11-24 23:02:10 +0300

    Updates

---
+++ precache-manifest.cebfc7babc4cd69a3835c4dda547f770.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff99a6fbd564cfead137df37a3d5fcc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0904e0403500a749cc",
+    "url": "/static/css/main.e6ae9259.chunk.css"
+  },
+  {
+    "revision": "2b6e02c4a2d8c12b4c82",
+    "url": "/static/js/2.747cd721.chunk.js"
+  },
+  {
+    "revision": "8d0df6a9f6b2d88ac068d142fd4b4030",
+    "url": "/static/js/2.747cd721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0904e0403500a749cc",
+    "url": "/static/js/main.fd1eed90.chunk.js"
+  },
+  {
+    "revision": "a73bd4a4ccd2b0a8d344",
+    "url": "/static/js/runtime-main.7862f35e.js"
+  },
+  {
+    "revision": "828d51f5bbda9bee1d844ee06f181309",
+    "url": "/static/media/Fobos.828d51f5.jpg"
+  },
+  {
+    "revision": "b863af801a3e2018a4f17d6c62f662f2",
+    "url": "/static/media/Therad100.b863af80.jpg"
+  },
+  {
+    "revision": "44c10b88b6f692e8027989d91b542fea",
+    "url": "/static/media/Therad200.44c10b88.jpg"
+  },
+  {
+    "revision": "7f2a2917bf5d24d0934d2c4f7f5d7166",
+    "url": "/static/media/Therad300.7f2a2917.jpg"
+  },
+  {
+    "revision": "006ff07478739078d1ce37b9fadea5a2",
+    "url": "/static/media/myPhoto.006ff074.jpg"
+  }
+]);
